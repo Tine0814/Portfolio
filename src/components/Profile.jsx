1fc44d938154fe3dc14bf63c5ba2cce7bb48b7f3
Tine14 <73092575+Tine0814@users.noreply.github.com>
2023-04-14 21:33:03 +0800

    04-14

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -10,9 +10,18 @@ const Hero = () => {
       >
         <div className="flex flex-col justify-center items-center mt-5">
           <div className="w-5 h-5 rounded-full bg-[#915eff]" />
-          <div className="w-1 h-40 s:80 violet-gradient" />
+          <div className="w-1 sm:h-80 h-40 violet-gradient" />
+        </div>
+        <div>
+          <h1 className={`${styles.heroHeadText} text-white`}>
+            Hi, I'm <span className="text-[#915eFF]">Dastine</span>
+          </h1>
+          <p className={`${styles.heroSubText} mt-auto text-white-100`}>
+            I develop web-application
+          </p>
         </div>
       </div>
+      <ComputersCanvas />
     </section>
   );
 };
